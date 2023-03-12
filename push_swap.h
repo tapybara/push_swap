3e2ada063685046164f19069933aa0ea60907141
tokuyama <okuyama0529work@gmail.com>
2023-03-12 21:52:22 +0900

    Add Define dlist

--- push_swap.h
+++ push_swap.h
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tokuyama <tokuyama@student.42.fr>          +#+  +:+       +#+        */
+/*   By: okuyamatakahito <okuyamatakahito@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 17:59:40 by tokuyama          #+#    #+#             */
-/*   Updated: 2023/03/11 20:22:52 by tokuyama         ###   ########.fr       */
+/*   Updated: 2023/03/12 14:10:29 by okuyamataka      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP.H
 # define PUSH_SWAP.H
 
-typedef struct  abc{
+typedef struct  dlist{
     int     data;
-    void    *prev;
-    void    *next;
-} abc;
+    struct dlist    *prev;
+    struct dlist    *next;
+} dlist;
 
 
 
